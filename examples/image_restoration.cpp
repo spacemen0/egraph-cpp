9b33e9c24ff2f67f1ad7875d9c15b4aece7e4e90
spacemen0 <frakctures@outlook.com>
2026-09-10 23:26:05 +0200

    refactor: restructure image restoration logic into separate contexts

--- examples/image_restoration.cpp
+++ examples/image_restoration.cpp
@@ -7,22 +7,6 @@
 using namespace egraph;
 
 int run_image() {
-    EGraphRunner::Context ctx;
-    ctx.get_config().enable_logging = false;
-    Expression y = ctx.define_matrix_symbolic("y", "a", 1);
-    Expression H = ctx.define_matrix_symbolic("H", "a", "b", {"full_rank", "wide"});
-    Expression I = ctx.define_matrix_symbolic("I", "b", "b", {"identity"});
-    Expression x = ctx.define_matrix_symbolic("x", "b", 1);
-
-    Expression math_1 = transpose(H) * inverse(H * transpose(H));
-    Expression math_2 = math_1 * y + (I - math_1 * H) * x;
-
-    auto id_1 = ctx.add(math_1);
-    auto id_2 = ctx.add(math_2);
-    ctx.initialize_config(math_2);
-    ctx.rewrite({"everything_but_lowering"});
-    ctx.lower_to_kernels();
-    // ctx.optimize_symbolic(math_2);
     auto [h_sizes, h_data] = read_matrix("examples/data/image_h.csv");
     auto [y_sizes, y_data] = read_matrix("examples/data/image_y.csv");
     auto [x_sizes, x_data] = read_matrix("examples/data/image_x.csv");
@@ -45,25 +29,48 @@ int run_image() {
     }
 
     SizeBindings concrete_sizes = {{"a", h_sizes.first}, {"b", h_sizes.second}};
-    DataBindings concrete_data = {
-        {"y", y_data}, {"H", h_data}, {"I", generate_identity_matrix(h_sizes.second)}, {"x", x_data}};
 
-    auto out2 = ctx.evaluate_concrete(id_2, concrete_sizes, concrete_data);
+    // Context 1: Optimize and evaluate math_1 (pseudo-inverse of wide H)
     {
-        auto out_shape = bind_shape(ctx.get_property(id_2).shape, &concrete_sizes);
-        int row = std::get<int>(out_shape.first);
-        int col = std::get<int>(out_shape.second);
-        std::string out_path2 = "examples/data/image_result_2.csv";
-        write_matrix(out_path2, row, col, out2);
-    }
+        EGraphRunner::Context ctx1;
+        ctx1.get_config().enable_logging = false;
+        Expression H1 = ctx1.define_matrix_symbolic("H", "a", "b", {"full_rank", "wide"});
+        Expression math_1 = transpose(H1) * inverse(H1 * transpose(H1));
 
-    auto out1 = ctx.evaluate_concrete(id_1, concrete_sizes, concrete_data);
-    {
-        auto out_shape = bind_shape(ctx.get_property(id_1).shape, &concrete_sizes);
-        int row = std::get<int>(out_shape.first);
-        int col = std::get<int>(out_shape.second);
+        ctx1.optimize_symbolic(math_1);
+
+        DataBindings concrete_data1 = {{"H", h_data}};
+        auto out1 = ctx1.evaluate_concrete(concrete_sizes, concrete_data1);
+        auto out1_shape = bind_shape(ctx1.get_property().shape, &concrete_sizes);
+        int row1 = std::get<int>(out1_shape.first);
+        int col1 = std::get<int>(out1_shape.second);
         std::string out_path1 = "examples/data/image_result_1.csv";
-        write_matrix(out_path1, row, col, out1);
+        write_matrix(out_path1, row1, col1, out1);
     }
+
+    // Context 2: Optimize and evaluate math_2 (image restoration formula)
+    {
+        EGraphRunner::Context ctx2;
+        ctx2.get_config().enable_logging = false;
+        Expression y = ctx2.define_matrix_symbolic("y", "a", 1);
+        Expression H2 = ctx2.define_matrix_symbolic("H", "a", "b", {"full_rank", "wide"});
+        Expression I = ctx2.define_matrix_symbolic("I", "b", "b", {"identity"});
+        Expression x = ctx2.define_matrix_symbolic("x", "b", 1);
+
+        Expression math_1_in_2 = transpose(H2) * inverse(H2 * transpose(H2));
+        Expression math_2 = math_1_in_2 * y + (I - math_1_in_2 * H2) * x;
+
+        ctx2.optimize_symbolic(math_2);
+
+        DataBindings concrete_data2 = {
+            {"y", y_data}, {"H", h_data}, {"I", generate_identity_matrix(h_sizes.second)}, {"x", x_data}};
+        auto out2 = ctx2.evaluate_concrete(concrete_sizes, concrete_data2);
+        auto out2_shape = bind_shape(ctx2.get_property().shape, &concrete_sizes);
+        int row2 = std::get<int>(out2_shape.first);
+        int col2 = std::get<int>(out2_shape.second);
+        std::string out_path2 = "examples/data/image_result_2.csv";
+        write_matrix(out_path2, row2, col2, out2);
+    }
+
     return 0;
 }
