7b124a4cb3a26d80dcdf9f8259876c2a7edef57a
spacemen0 <frakctures@outlook.com>
2026-09-11 13:31:29 +0200

    refactor: remove unused GLSSymbolic test case from integration tests

--- tests/integration/gls_test.cpp
+++ tests/integration/gls_test.cpp
@@ -28,26 +28,3 @@ TEST(Integration, GLSNumeric) {
     }
     std::cout << "Num nodes after rewriting: " << egraph.num_nodes() << std::endl;
 }
-
-TEST(Integration, GLSSymbolic) {
-    PropertyTable pt;
-    pt.add_or_update_property_entry(
-        "X", {.shape = std::make_pair("A", "B"), .flags = {.is_full_rank = true, .is_tall = true}});
-    pt.add_or_update_property_entry(
-        "M", {.shape = std::make_pair("A", "A"), .flags = {.is_symmetric = true, .is_positive_definite = true}});
-    pt.add_or_update_property_entry("y", {.shape = std::make_pair("A", 1)});
-    EGraph egraph(pt);
-    auto id = egraph.add_expression(Expression("(Inv(Tr(X) * Inv(M) * X) * (Tr(X) * Inv(M))) * y"));
-
-    std::vector<Rewrite> rules = build_rewrite_sets({"complete"});
-    Rewriter rewriter(egraph, rules, EGraphConfig{.rewrite = {.node_limit = 1000, .enable_backoff = true}});
-    while (rewriter.apply_rewrites(10))
-        ;
-    Extractor extractor(egraph, EGraphConfig{.enable_logging = true});
-    auto result = extractor.extract(id, 10, {{"A", 100}, {"B", 20}});
-    for (const auto &r : result) {
-        std::cout << "Candidate expression: " << r.expr.to_string(true) << std::endl;
-        std::cout << "Cost: " << r.cost << std::endl;
-    }
-    std::cout << "Num nodes after rewriting: " << egraph.num_nodes() << std::endl;
-}
